db6849bfa33f13c92a5ed5afb1da6f0fa65d6c09
YDan08 <dasyamashita@gmail.com>
2022-04-27 21:19:41 -0300

    feat: criar projeto progamer

---
+++ src/main/java/br/com/fiap/model/User.java
@@ -0,0 +1,47 @@
+package br.com.fiap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome = "Daniel yamashita";
+	private String email = "daniel@gmail.com";
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "User [nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+	}
+	
+	
+	
+}
